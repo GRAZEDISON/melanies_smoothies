de8ccce868f952b0dbe20c4d8a135265c3695920
GRAZEDISON <Edison.Widell@graz.se>
2024-11-05 11:31:14 +0100

    Update streamlit_app.py
    For some reason the snowflake download wasnt up to date

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,17 +19,22 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'choose up to 5 ingridients:'
     , my_dataframe
+    , max_selections=5
 )
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
     st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+    st.write(my_insert_stmt)
+    #st.stop()
 
     time_to_insert = st.button('Submit Order')
+
+    
     #st.write(my_insert_stmt)
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+        st.success('Your Smoothie is ordered! ' +name_on_order, icon="✅")
